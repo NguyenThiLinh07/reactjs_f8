86dc051afb2a8293121d67946aa0229527e8a932
Linh <leeminho03072001@gmail.com>
2022-09-26 13:16:36 +0700

    update

--- Ôn lại ES6+/bt2.7.ts
+++ Ôn lại ES6+/bt2.7.ts
@@ -1,14 +1,17 @@
 // Cho trước mảng infoArr, hãy viết hàm arrToObj để chuyển array thành object
+type Result = {
+    [index: string]: number|string;
+}
 
-const infoArr:any[] = [
+const infoArr:((number|string)[])[] = [
     ['name', 'Son Dang'], 
     ['age', 21], 
     ['address', 'Ha Noi']
 ];
 
-const arrToObj = (arr: any[]) => arr.reduce((acc, cur) => {
-    acc[cur[0]] = cur[1];
-    return acc;
+const arrToObj = (arr:((number|string)[])[]) => arr.reduce((acc:Result, cur:(number|string)[]) :Result=> {
+        console.log(acc);
+        return {...acc, [cur[0]]:cur[1]}
 }, {});
 
 console.log(arrToObj(infoArr));
